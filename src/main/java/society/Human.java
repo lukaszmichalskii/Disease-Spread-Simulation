90c4167a6c4b0be8e7d8a5ba723ab2cadfa6d8b8
Lukasz Michalski <261118@student.pwr.edu.pl>
2021-04-17 16:16:16 +0200

    Add Vector class and update Human

--- src/main/java/society/Human.java
+++ src/main/java/society/Human.java
@@ -1,4 +1,13 @@
 package society;
 
+import tools.Vector;
+
 public class Human {
+    Vector position;
+    Vector velocity;
+
+    public Human() {
+        position = new Vector();
+        velocity = new Vector();
+    }
 }
---
+++ src/main/java/tools/Vector.java
@@ -0,0 +1,16 @@
+package tools;
+
+public class Vector {
+
+    double x, y;
+
+    public Vector() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
