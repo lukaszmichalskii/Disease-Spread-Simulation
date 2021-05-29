40d1ff2e9f795075839637f1d6201aef70801992
Lukasz Michalski <261118@student.pwr.edu.pl>
2021-05-29 14:25:29 +0200

    Documentation comments

--- src/main/java/tools/Map.java
+++ src/main/java/tools/Map.java
@@ -10,16 +10,28 @@ import static gui.Screen.WIN_HEIGHT;
 import static gui.Screen.WIN_WIDTH;
 import static society.Logic.maxSpeed;
 
+/**
+ * Class create and stores position, velocity and acceleration of society objects.
+ * @author Lukasz Michalski
+ */
 public class Map {
 
     private java.util.Map<Human, java.util.Map<String, Vector>> map;
 
-
+    /**
+     * Constructor
+     * @param SIZE
+     */
     public Map(int SIZE) {
         ArrayList<java.util.Map<String, Vector>> movement = attributesCreator(SIZE);
         map = mapCreator(movement);
     }
 
+    /**
+     * Create map of human movement attributes
+     * @param attributesStack array of dictionaries with position, velocity and acceleration values
+     * @return complete map / null
+     */
     private java.util.Map<Human, java.util.Map<String, Vector>> mapCreator(ArrayList<java.util.Map<String, Vector>> attributesStack) {
         if (attributesStack != null) {
             map = new HashMap<>();
@@ -33,6 +45,11 @@ public class Map {
         return null;
     }
 
+    /**
+     * Create dictionaries, every store unique movement parameters of society objects
+     * @param SIZE number of dictionaries (every human different movement values)
+     * @return array of dictionaries / null
+     */
     private ArrayList<java.util.Map<String, Vector>> attributesCreator(int SIZE) {
         if (SIZE > 0) {
             ArrayList<java.util.Map<String, Vector>> attributesStack = new ArrayList<>();
@@ -50,26 +67,56 @@ public class Map {
         return null;
     }
 
+    /**
+     * Method get position vector of human from map
+     * @param human analyze object (key to dictionary)
+     * @return position vector of object
+     */
     public Vector getPosition(Human human) {
         return map.get(human).get("position");
     }
 
+    /**
+     * Method set new position to human and save in map
+     * @param human analyze object (key to dictionary)
+     * @param position new position vector to set
+     */
     public void setPosition(Human human, Vector position) {
         map.get(human).put("position", position);
     }
 
+    /**
+     * Method get velocity vector of human from map
+     * @param human analyze object (key to dictionary)
+     * @return velocity vector of object
+     */
     public Vector getVelocity(Human human) {
         return map.get(human).get("velocity");
     }
 
+    /**
+     * Method set new velocity to human and save in map
+     * @param human analyze object (key to dictionary)
+     * @param velocity new velocity vector to set
+     */
     public void setVelocity(Human human, Vector velocity) {
         map.get(human).put("velocity", velocity);
     }
 
+    /**
+     * Method get acceleration vector of human from map
+     * @param human analyze object (key to dictionary)
+     * @return acceleration vector of object
+     */
     public Vector getAcceleration(Human human) {
         return map.get(human).get("acceleration");
     }
 
+    /**
+     * Method set new acceleration to human and save in map
+     * @param human analyze object (key to dictionary)
+     * @param acceleration new acceleration vector to set
+     */
     public void setAcceleration(Human human, Vector acceleration) {
         map.get(human).put("acceleration", acceleration);
     }
